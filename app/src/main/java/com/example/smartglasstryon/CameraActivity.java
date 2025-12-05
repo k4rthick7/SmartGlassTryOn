11be751adf78df5f666e372485744573d16a72fa
k4rthick07 <karthicknathan333@gmail.com>
2025-12-05 14:51:19 +0530

    feat(ui): add main screen and navigation

---
+++ app/src/main/java/com/example/smartglasstryon/CameraActivity.java
@@ -0,0 +1,14 @@
+package com.example.smartglasstryon;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // We will create this layout file next if it's empty
+        setContentView(R.layout.activity_camera);
+    }
+}
